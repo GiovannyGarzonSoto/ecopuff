873e329b4d38823205525b76a6dcfc306f843c47
Giovanky <giovankyandres@gmail.com>
2019-08-28 18:22:22 -0400

    final commit

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://user:user@cluster0-hovsi.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://finaluser:user@cluster0-hovsi.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true
 })
 .then(db =>  console.log('Base de datos conectada'))
